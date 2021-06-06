ca39d0a53be13ac3f7e6f49ed0f666cc3dcbc350
sanjayprasadF5 <sanjayprasad2321998@gmail.com>
2021-06-06 14:29:59 +0530

    basic js 2 console based done

---
+++ Javascript Assignment/JavaScript Logical Assignment/Javascript Basic js --2/Basic js 2/main.js
@@ -0,0 +1,15 @@
+function javascript() {
+  console.log("hello javascript world!");
+}
+var num;
+function isEven(num) {
+  if (num) {
+    if (num % 2 == 0) {
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    console.log("Not found!");
+  }
+}
